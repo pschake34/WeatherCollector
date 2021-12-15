8965052fc57c2f575cf741605b0b4bee3ccd99bd
pschake34 <pschake34@charlottesvilleschools.org>
2021-12-15 15:45:36 -0500

    Create initial prototype of wind speed sensor

---
+++ prototypes/wind_speed/wind_speed.ino
@@ -0,0 +1,32 @@
+// Paul Schakel & Ezhar Zahid
+// Wind Speed
+// Prototype testing out an Adafruit wind sensor 
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Initialized...");
+}
+
+void loop() {
+    float sensorValue = analogRead(A1);
+    Serial.print("Analog Value =");
+    Serial.println(sensorValue);
+    delay(500);
+
+    float voltage = (sensorValue / 960) * 5;
+    Serial.print("Voltage: ");
+    Serial.print(voltage);
+    Serial.println("V");
+
+    float wind_speed = mapfloat(voltage, 0.4, 2, 0, 32.4);
+    float speed_mph = ((wind_speed *3600)/1609.344);
+    Serial.print("Wind Speed =");
+    Serial.print(wind_speed);
+    Serial.println("m/s");
+    Serial.print(speed_mph);
+    Serial.println("mph");
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
