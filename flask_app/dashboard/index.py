1767e2065a410b38dccfb8b765711aa8fbf25dac
pschake34 <pschake34@charlottesvilleschools.org>
2022-02-02 23:12:45 -0500

    Refactor web app into fewer files

---
+++ flask_app/dashboard/index.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import (
+    Flask, Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+# create and configure the app
+app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'dashboard.sqlite'),
+)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+import db
+db.init_app(app)
+
+@app.route('/')
+def home():
+    database = db.get_db()
+    return render_template('home.html')
+
+@app.route('/graphs')
+def graphs():
+    return render_template('graphs.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
