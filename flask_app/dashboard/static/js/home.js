9bcad25b29405b31acf10cc5ba6236e6de07ff0c
pschake34 <pschake34@charlottesvilleschools.org>
2022-02-09 15:54:55 -0500

    Make changes to compute coordinates for temperature guage

--- flask_app/dashboard/static/js/home.js
+++ flask_app/dashboard/static/js/home.js
@@ -1,5 +1,32 @@
-let root = document.documentElement;
+var root = document.querySelector(':root');
+var lowerLimit = -20;
+var upperLimit = 120;
+var u = upperLimit - lowerLimit;
+var radius = 60;
+var transform = radius / 3;
 
-function setTempGuage() {
-    root.style.setProperty('--pointerleft', temperature);
+function toRadians (angle) {
+    return angle * (Math.PI / 180);
+}
+
+function computeY(temp) {
+    return -(radius - change) * Math.cos(toRadians(((temp - lowerLimit) / u) * 360)) + transform;
+}
+
+function computeX(temp) {
+    x = Math.sqrt((radius*radius)-(temp*temp));
+    if (temp > (upperLimit-lowerLimit)) {
+        return x;
+    } else {
+        return -1 * x;
+    }
+}
+
+function computeAngle(temp) {
+    null;
+}
+
+function setTempGuage(temp) {
+    root.style.setProperty('--pointertop', computeY(80) + "%");
+    root.style.setProperty('--pointerleft', computeX(80) + "%");
 }
