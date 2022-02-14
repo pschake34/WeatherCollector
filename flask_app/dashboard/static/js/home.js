8c9ed23cbdce4f5b1e8ef921d7b4fb5390c3eb47
pschake34 <pschake34@charlottesvilleschools.org>
2022-02-13 22:14:51 -0500

    Add functionality for getting data from webhooks and celcius temperatures

--- flask_app/dashboard/static/js/home.js
+++ flask_app/dashboard/static/js/home.js
@@ -35,15 +35,45 @@ function computeAngle(temp, y) {
     }
 }
 
-function setTempGuage() {
-    y = computeY(temperature);
-    x = computeX(temperature, y);
-    angle = computeAngle(temperature, y);
+function setTempGuage(temp) {
+    y = computeY(temp);
+    x = computeX(temp, y);
+    angle = computeAngle(temp, y);
     console.log("X: " + x + "\nY: " + y + "\nDeg: " + angle);
-    console.log(temperature);
+    console.log(temp);
     root.style.setProperty('--pointertop', 50 - y + "%");
     root.style.setProperty('--pointerleft', 47 + x + "%");
     root.style.setProperty('--pointerdeg', 90-angle + "deg");
+}
 
-    setTimeout(setTempGuage, 40);
+async function getData(type) {
+    let url = '/get_sensor_data/' + type;    
+    let obj = null;
+    
+    try {
+        obj = await (await fetch(url)).json();
+    } catch(e) {
+        console.log('error');
+    }
+
+    return Object.values(obj)[0];
+}
+
+async function main() {
+    let url = "/get_sensor_data";
+    var data;
+
+    fetch(url + "/temperature")
+    .then(res => res.json())
+    .then(out => data = out)
+    .catch(err => console.log(err));
+
+    var temperatureF = await getData("temperature");
+    var temperatureC = (temperatureF - 32) * (5/9);
+    setTempGuage(temperatureF)
+    document.getElementById("temperature-F").innerHTML = temperatureF.toFixed(2) + "°F";
+    document.getElementById("temperature-C").innerHTML = temperatureC.toFixed(2) + "°C";
+
+
+    setTimeout(main, 10000);  //run every 10 seconds
 }
