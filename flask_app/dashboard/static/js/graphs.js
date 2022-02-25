b2205e3aebc0109b795c740dfdc190c801e59053
pschake34 <pschake34@charlottesvilleschools.org>
2022-02-24 23:12:03 -0500

    Finish dynamic graph creation js

--- flask_app/dashboard/static/js/graphs.js
+++ flask_app/dashboard/static/js/graphs.js
@@ -2,15 +2,32 @@ var activeElements = document.getElementsByClassName("active");
 var timespan = activeElements[1].id;
 var datatype = activeElements[2].id;
 
+async function getGraphData(datatype, timespan) {  //gets the weather data from the server webhook and extracts the value from json
+    let url = '/get_graph_data/' + timespan + '/' + datatype;
+    let obj = null;
+    
+    try {
+        obj = await (await fetch(url)).json();
+    } catch(e) {
+        console.log('error');
+    }
+    
+    return [Object.values(obj)[0], Object.values(obj)[1]];
+}
+
+var data = await getGraphData(datatype, timespan);
+var times = data[0];
+var datapoints = data[1];
+
 const ctx = document.getElementById('chart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: "line",
     data: {
-        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        labels: times,
         datasets: [
             {
-                label: "Dataset 1",
-                data: [1, 2, 5, 4, 6, 10, 5, 8, 2],
+                label: datatype,
+                data: datapoints,
                 borderColor: ["rgba(255, 0, 0, 1)"],
                 backgroundColor: ["rgba(255, 0, 0, 0.2)"],
             }
