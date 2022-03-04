da34b8d22359a8684d3934a03c990d470c9083f6
pschake34 <pschake34@charlottesvilleschools.org>
2022-03-04 10:22:39 -0500

    Finish rewriting graph webhook and add a bunch of comments

--- flask_app/dashboard/__init__.py
+++ flask_app/dashboard/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='SECRET_KEY',
         DATABASE=os.path.join(app.instance_path, 'dashboard.sqlite'),
     )
 
@@ -33,10 +33,10 @@ def create_app(test_config=None):
     valid_types = ["temperature", "humidity", "pressure", "windSpeed"]
     valid_timespans = ["year", "half_year", "month", "week", "day", "hour"]
 
-    # Utility function for date conversion
+    # Utility function for date conversion on the production server
     def datetime_from_utc_to_local(utc_datetime):
         now_timestamp = time.time()
-        offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp) # - timedelta(hours=5)
+        offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp) - timedelta(hours=5)
         return utc_datetime + offset
 
     # Webhooks
@@ -47,9 +47,9 @@ def create_app(test_config=None):
             json_object = json.loads(json.dumps(request_json))
             database = db.get_db()
 
-            if valid_types.index(json_object["name"]) != -1:
+            if valid_types.index(json_object["name"]) != -1: # checks if type is valid
                 try:
-                    database.execute(
+                    database.execute( # saves value in database
                         "INSERT INTO {} (value) VALUES (?)".format(json_object["name"]),
                         (json_object["value"],)
                     )
@@ -66,9 +66,9 @@ def create_app(test_config=None):
         data = {"value": 0}
         database = db.get_db()
 
-        if valid_types.index(type) != -1:
+        if valid_types.index(type) != -1: # checks if datatype is valid
             database_table = type
-            database_values = database.execute(
+            database_values = database.execute( # gets most recent value from database
                 "SELECT * FROM {} ORDER BY time DESC LIMIT 0, 1".format(database_table)
             ).fetchone()
             data["value"] = database_values["value"]
@@ -77,15 +77,16 @@ def create_app(test_config=None):
         return data, 200
 
     @app.route('/get_graph_data/<timespan>/<datatype>', methods=['GET'])
-    def get_graph_data(timespan, datatype):
-        data = {"values": [], "times": []}
-        day_data = []
+    def get_graph_data(timespan, datatype): # sends the data for a graph of the specified timspan and datatype
+        data = {"values": [], "times": []} # used to store data for any timespan
+        day_data = [] # used to temporarily store data separated by day when timespan > 1 day
         database = db.get_db()
 
-        if valid_timespans.index(timespan) > -1:
-            if valid_types.index(datatype) > -1:
+        if valid_timespans.index(timespan) > -1: # check if timespan is valid
+            if valid_types.index(datatype) > -1: # check if datatype is valid
                 database_table = datatype
 
+                # change the timespan phrasing so they can be used in the db query
                 if timespan == "half_year":
                     timespan = "6 months"
                 elif timespan == "week":
@@ -93,35 +94,30 @@ def create_app(test_config=None):
                 else:
                     timespan = "1 " + timespan
 
-                database_values = database.execute(
+                database_values = database.execute( # get data from database
                     "SELECT * FROM {} WHERE time > datetime('now', '-{}') ORDER BY time ASC".format(database_table, timespan)
                 ).fetchall()
 
-                if timespan != "1 hour" and timespan != "1 day":
-                    current_day = datetime(1970, 1, 1).date()
+                if timespan != "1 hour" and timespan != "1 day": # if timespan is year, half_year, month, or week, sort data by day
+                    current_day = datetime_from_utc_to_local(database_values[0]["time"]).date() # first date in database values
                     current_day_data = {"values": [], "times": []}
                     i = 0
-                    while i > len(database_values):
+                    while i < len(database_values):
                         temp_day = datetime_from_utc_to_local(database_values[i]["time"]).date()
-                        if temp_day > current_day and i == 0:
-                            current_day = temp_day
-                        elif temp_day > current_day and i > 0:
-                            current_day_data["values"].append(database_values[i]["value"])
-                            current_day_data["times"].append(datetime_from_utc_to_local(database_values[i]["time"]))
+                        if temp_day > current_day: # store values for that day in day_data array
                             day_data.append(current_day_data)
                             current_day_data = {"values": [], "times": []}
                             current_day = temp_day
-                        else:
-                            current_day_data["values"].append(database_values[i]["value"])
-                            current_day_data["times"].append(datetime_from_utc_to_local(database_values[i]["time"]))
+                        current_day_data["values"].append(database_values[i]["value"])
+                        current_day_data["times"].append(datetime_from_utc_to_local(database_values[i]["time"]))
                         i += 1
                 
-                if timespan == "1 year" or timespan == "6 months" or timespan == "1 month":
+                if timespan == "1 year" or timespan == "6 months" or timespan == "1 month": # average data by day
                     day_cnt = 0
                     day_total = 0
-                    current_day = datetime(1970, 1, 1).date()
+                    current_day = datetime(1970, 1, 1).date() # just temporary initial value
 
-                    for day in day_data:
+                    for day in day_data: # store average value of each day in data dict
                         i = 0
                         current_day = day["times"][i].date()
 
@@ -133,42 +129,45 @@ def create_app(test_config=None):
                         data["values"].append(day_total/day_cnt)
                         day_cnt = 0
                         day_total = 0
-                elif timespan == "7 days":
+                elif timespan == "7 days": # average data for morning and afternoon every day
                     half_day_cnt = 0
                     half_day_total = 0
-                    initial_day_extension = "AM"
-                    current_day = datetime(1970, 1, 1, 0, 0, 0)
+                    half_day_past = True
+                    current_day = datetime(1970, 1, 1).date() # just temporary initial value
 
-                    for day in day_data:
+                    for day in day_data: # store average morning and afternoon values in data dict
                         i = 0
                         current_day = day["times"][i].date()
-
-                        if day["times"][0].hour() < 12:
-                            initial_day_extension = "AM"
+                        if day["times"][0].hour < 12: # checks whether graph start is in morning or afternoon
+                            half_day_past = False
                         else:
-                            initial_day_extension = "PM"
+                            half_day_past = True
 
-                        while i < len(day):
-                            if day["times"][0].hour() > 12 and initial_day_extension != "PM":
+                        while i < len(day["values"]):
+                            if day["times"][i].hour > 12 and not half_day_past: # log data for morning
                                 data["times"].append(current_day.strftime("%m/%d/%Y - AM"))
                                 data["values"].append(half_day_total/half_day_cnt)
                                 half_day_cnt = 0
                                 half_day_total = 0
+                                half_day_past = True
                             half_day_total += day["values"][i]
                             half_day_cnt += 1
-
+                            i += 1
+                        
+                        if day["times"][-1].hour < 12: # log data for morning if graph ends in morning
+                            data["times"].append(current_day.strftime("%m/%d/%Y - AM"))
+                        else: # log data for afternoon
                             data["times"].append(current_day.strftime("%m/%d/%Y - PM"))
-                            data["values"].append(half_day_total/half_day_cnt)
-                            half_day_cnt = 0
-                            half_day_total = 0
+                        data["values"].append(half_day_total/half_day_cnt)
+                        half_day_cnt = 0
+                        half_day_total = 0
                 else:   # for day and month range
                     data["values"] = [value["value"] for value in database_values]
                     data["times"] = [datetime_from_utc_to_local(value["time"]).strftime("%H:%M") for value in database_values]
-            else:
+            else: # throw error if datatype is invalid
                 return 'Not a valid type', 400
-        else:
+        else: # throw error if timspan is invalid
             return 'Not a valid timespan', 400
-        print(data)
         return data, 200
 
     # Visible web pages
@@ -177,18 +176,18 @@ def create_app(test_config=None):
         return render_template('home.html')
 
     @app.route('/graphs')
-    def graph_redirect():
+    def graph_redirect(): # redirects to the temperature graph of the last hour
         return redirect('/graphs/hour/temperature')
 
     @app.route('/graphs/<timespan>/<datatype>')
-    def graphs(timespan, datatype):
-        timespan_active = ["", "", "", "", "", ""]
+    def graphs(timespan, datatype): # displays a graph with the specified timespan and datatype
+        timespan_active = ["", "", "", "", "", ""] # used to show which graph is active
         datatype_active = ["", "", "", ""]
         timespan_index = valid_timespans.index(timespan)
         datatype_index = valid_types.index(datatype)
 
-        if timespan_index > -1:
-            if datatype_index > -1:
+        if timespan_index > -1: # check if the timespan is valid
+            if datatype_index > -1: # check if the datatype is valid
                 timespan_active[timespan_index] = "active"
                 datatype_active[datatype_index] = "active"
             else:
